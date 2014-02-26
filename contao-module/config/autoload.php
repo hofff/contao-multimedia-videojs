758d91aac0ae6a60f5346addbe7177bf2fa0005e
Oliver Hoff <oliver@hofff.com>
2014-02-26 17:24:21 +0100

    add autoload files for contao 3

---
+++ contao-module/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Backboneit_multimedia_videojs
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'bbit_mm_vjs' => 'system/modules/backboneit_multimedia_videojs/templates',
+));
